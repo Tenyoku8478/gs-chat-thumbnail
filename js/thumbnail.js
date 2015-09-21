7b762e806523ccad8b250745e099d47babe8a170
Tenyoku <sh961014@gmail.com>
2015-09-21 23:00:25 +0800

    v0.0.3.2 grab image from site/online

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -12,18 +12,41 @@ function getThumbnailUrl(userId, callback) {
                     url='/user/'+userId+'/',
                     success = function(data, state, jqXHR) {
                         var json = $(data).find('script:contains(thumbnail)').text();
-                        var obj = JSON.parse(json);
-                        thumbnail = obj.feeds[0].subList[0].user.thumbnail;
-                        var list = {};
-                        list[name] = thumbnail;
-                        chrome.storage.local.set(list,
-                            function() {
-                                for(var i=0; i<userIdQueue.length; i+=1) {
-                                    userIdQueue[i](thumbnail);
+                        if(json) {
+                            var obj = JSON.parse(json);
+                            thumbnail = obj.feeds[0].subList[0].user.thumbnail;
+                            var list = {};
+                            list[name] = thumbnail;
+                            chrome.storage.local.set(list,
+                                function() {
+                                    for(var i=0; i<userIdQueue.length; i+=1) {
+                                        userIdQueue[i](thumbnail);
+                                    }
+                                    queues[userId] = [];
                                 }
-                                queues[userId] = [];
-                            }
-                        );
+                            );
+                        }
+                        else {
+                            $.get(
+                                url='/site/online/',
+                                success = function(data, state, jqXHR) {
+                                    thumbnail = $(data).find('.user_icon[href="user/'+userId+'/"]').css('background-image');
+                                    if(thumbnail) {
+                                        thumbnail = thumbnail.substring(4, thumbnail.length-1);
+                                        var list = {};
+                                        list[name] = thumbnail;
+                                        chrome.storage.local.set(list,
+                                            function() {
+                                                for(var i=0; i<userIdQueue.length; i+=1) {
+                                                    userIdQueue[i](thumbnail);
+                                                }
+                                                queues[userId] = [];
+                                            }
+                                        );
+                                    }
+                                }
+                            );
+                        }
                     }
                 );
             }
