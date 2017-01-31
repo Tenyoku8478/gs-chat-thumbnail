48c710b0a290f6c5ba4e86a52d414e578751f848
tenyoku <tenyoku@tenyoku-mac.local>
2017-01-31 15:55:20 +0800

    promise

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,71 +1,79 @@
-var queues = {}
-function getThumbnailUrl(userId, callback) {
-  // use queue to buffer requests for same user
-  var userIdQueue = queues[userId];
-  if(!userIdQueue) queues[userId] = userIdQueue = [];
-  userIdQueue.push(callback);
-  if(userIdQueue.length == 1) { // if this is the only element in queue, get thumbnail
-    var name = 'thumbnail/'+userId;
-    chrome.storage.local.get(name, (obj) => { // try to get the thumbnail in storage
-      var thumbnail = obj[name];
-      if(!thumbnail) { // if thumbnail is not cached in storage
-        $.get(
-          url='/user/'+userId+'/',
-          success = (data, state, jqXHR) => {
-            var json = $(data).find('script:contains(thumbnail)').text();
-            if(json) { // if has status on profile page
-              var obj = JSON.parse(json);
-              thumbnail = obj.feeds[0].subList[0].user.thumbnail;
-              var list = {};
-              list[name] = thumbnail;
-              chrome.storage.local.set(list, // save to storage, and then call all callbacks in the queue
-                () => {
-                  for(var i=0; i<userIdQueue.length; i+=1) {
-                    userIdQueue[i](thumbnail); // callback, is likly attaching thumbnail to webpage
-                  }
-                  queues[userId] = [];
-                }
-              );
-            }
-            else { // if not, get thumbnail from online page
-              $.get(
-                url='/site/online/',
-                success = (data, state, jqXHR) => {
-                  thumbnail = $(data).find('.user_icon[href="user/'+userId+'/"]').css('background-image');
-                  if(thumbnail) {
-                    thumbnail = thumbnail.substring(4, thumbnail.length-1);
-                    var list = {};
-                    list[name] = thumbnail;
-                    chrome.storage.local.set(list,
-                      () => {
-                        for(var i=0; i<userIdQueue.length; i+=1) {
-                          userIdQueue[i](thumbnail);
-                        }
-                        queues[userId] = [];
-                      }
-                    );
-                  } // else I have no idea where I can get the thumbnail QAQ
-                }
-              );
-            }
-          }
-        );
-      }
-      else {
-        for(var i=0; i<userIdQueue.length; i+=1) {
-          userIdQueue[i](thumbnail);
-        }
-        queues[userId] = [];
-      }
-    });
+function triggerCallbacks(callbacks, thumbnail) {
+  for(let i=0; i<callbacks.length; i+=1) {
+    callbacks[i](thumbnail); // callback, is likly attaching thumbnail to webpage
   }
+  callbacks.length = 0;
 }
+
+function getThumbnailFromUser(userId) {
+  return new Promise((resolve, reject) => {
+    $.get(url='/user/'+userId+'/', success=(data) => {
+      const json = $(data).find('script:contains(thumbnail)').text();
+      if(!json) resolve(null);
+      const thumbnail = JSON.parse(json).feeds[0].subList[0].user.thumbnail;
+      resolve(thumbnail);
+    });
+  });
+}
+
+function getThumbnailFromOnline(userId) {
+  return new Promise((resolve, reject) => {
+    $.get(url='/site/online/', success=(data) => {
+      let thumbnail = $(data).find('.user_icon[href="user/'+userId+'/"]').css('background-image');
+      if(!thumbnail) resolve(null);
+      thumbnail = thumbnail.substring(4, thumbnail.length-1);
+      resolve(thumbnail);
+    });
+  });
+}
+
+function cacheThumbnail(name, thumbnail) {
+  return new Promise((resolve, reject) => {
+    var list = {};
+    list[name] = thumbnail;
+    chrome.storage.local.set(list, resolve);
+  });
+}
+
+const queues = {}
+function getThumbnailUrl(userId, callback) {
+  // use queue to buffer requests for the same user
+  let userIdQueue = queues[userId];
+  if(!userIdQueue) userIdQueue = queues[userId] = [];
+  userIdQueue.push(callback);
+
+  // not first call: wait for the first call trigger callbacks
+  if(userIdQueue.length > 1) return;
+
+  const name = 'thumbnail/'+userId;
+  // try to get the thumbnail in storage
+  chrome.storage.local.get(name, (obj) => {
+    const thumbnail = obj[name];
+    if(thumbnail) triggerCallbacks(userIdQueue, thumbnail);
+    else {
+      getThumbnailFromUser(userId)
+        .then((thumbnail) => {
+          if(!thumbnail) return getThumbnailFromOnline(userId);
+          else return thumbnail;
+        })
+        .then((thumbnail) => {
+          if(!thumbnail) return 'http://gameschool.cc/images/user.jpg';
+          else return thumbnail;
+        })
+        .then((thumbnail) => cacheThumbnail(name, thumbnail))
+        .then((thumbnail) => triggerCallbacks(userIdQueue, thumbnail))
+    }
+  });
+}
+
 function attachThumbnail() {
   // select not thumbnailed messages
-  $('.msgrow:has(.userID):not(.thumbnailed):not(.thumbnailing)').each(() => {
-    var $this = $(this);
-    $this.addClass('thumbnailing'); // lock to prevent duplicate attach
-    var userId = $this.find('.userID').attr('data-id'); // get user id from .userID
+  $('.msgrow:has(.userID):not(.thumbnailed):not(.thumbnailing)').each(function() {
+    const $this = $(this);
+    // lock the item to prevent duplicate attach
+    $this.addClass('thumbnailing');
+    // get user id from .userID
+    const userId = $this.find('.userID').attr('data-id');
     getThumbnailUrl(userId, (thumbnail) => {
       // callback: add img tag to message
       $this.find('.info').after('<img src="'+thumbnail+'" />');
